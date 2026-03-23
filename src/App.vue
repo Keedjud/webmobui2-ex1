1200212ada5e1153a52f5e444860653e3ec179c8
Keedjud <inoewenger@gmail.com>
2026-03-23 14:24:20 +0100

    Remove unused SVG files and implement temperature conversion in App.vue with BaseInputNumber component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <script setup>
-import { ref } from 'vue';
-import TheHeader from './components/TheHeader.vue';
+import { computed, ref } from "vue";
+import TheHeader from "./components/TheHeader.vue";
+import BaseInputNumber from "./components/BaseInputNumber.vue";
 
-  const temperature = ref(0);
+const temperatureSi = ref(0); //En Kelvin
+
+const tempKelvin = defineModel("kelvin", {
+    get: () => {
+        return temperatureSi.value.toFixed(2);
+    },
+    set: (value) => {
+        temperatureSi.value = Number(value);
+    },
+});
+
+const tempCelsius = defineModel("celsius", {
+    get: () => {
+        return (temperatureSi.value - 273.15).toFixed(2);
+    },
+    set: (value) => {
+        temperatureSi.value = Number(value + 273.15);
+    },
+});
+
+const tempFahrenheit = defineModel("fahrenheit", {
+    get: () => {
+        return (((temperatureSi.value - 273.15) * 9) / 5 + 32).toFixed(2);
+    },
+    set: (value) => {
+        temperatureSi.value = Number(((value - 32) * 5) / 9 + 273.15);
+    },
+});
 </script>
 
 <template>
-  <TheHeader />
-  <p>Temperature: {{ temperature }}K</p>
+    <TheHeader />
+    <section>
+        <BaseInputNumber label="Temperature in Kelvin" v-model="tempKelvin" />
+        <BaseInputNumber label="Temperature in Celsius" v-model="tempCelsius" />
+        <BaseInputNumber label="Temperature in Fahrenheit" v-model="tempFahrenheit" />
+    </section>
 </template>
+
+<style>
+  section {
+    display: grid;
+    margin: 2rem;
+    gap: 1rem;
+  }
+</style>
