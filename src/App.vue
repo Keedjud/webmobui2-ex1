2ee6de5f137c70fd977900c196732beff6dc183d
Keedjud <inoewenger@gmail.com>
2026-03-23 16:25:39 +0100

    Refactor App.vue and PageTemperature.vue: streamline temperature conversion logic and enhance layout

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,31 @@
 <script setup>
-import { computed, ref } from "vue";
 import TheHeader from "./components/TheHeader.vue";
-import BaseInputNumber from "./components/BaseInputNumber.vue";
 import { useHashRoute } from "./composable/useHashRoute.js";
 import Home from "./pages/PageHome.vue";
 import Temperature from "./pages/PageTemperature.vue";
 
 const routes = {
-    'home': Home,
-    'temperature': Temperature
+    home: Home,
+    temperature: Temperature,
 };
 
 const { currentPage } = useHashRoute(routes);
-const temperatureSi = ref(0); //En Kelvin
-
-const tempKelvin = defineModel("kelvin", {
-    get: () => parseFloat(temperatureSi.value.toFixed(2)),
-    set: (value) => temperatureSi.value = Number(value),
-});
-
-const tempCelsius = defineModel("celsius", {
-    get: () => parseFloat((temperatureSi.value - 273.15).toFixed(2)),
-    set: (value) => temperatureSi.value = Number(value + 273.15),
-});
-
-const tempFahrenheit = defineModel("fahrenheit", {
-    get: () => parseFloat((((temperatureSi.value - 273.15) * 9) / 5 + 32).toFixed(2)),
-    set: (value) => temperatureSi.value = Number(((value - 32) * 5) / 9 + 273.15),
-});
 </script>
 
 <template>
     <TheHeader />
     <main>
-      <Home v-if="currentPage === 'home'"/>
-      <Temperature v-if="currentPage === 'temperature'" />
+        <Home v-if="currentPage === 'home'" />
+        <Temperature v-if="currentPage === 'temperature'" />
     </main>
-    <section>
-        <BaseInputNumber label="Temperature in Kelvin" v-model="tempKelvin" />
-        <BaseInputNumber label="Temperature in Celsius" v-model="tempCelsius" />
-        <BaseInputNumber label="Temperature in Fahrenheit" v-model="tempFahrenheit" />
-    </section>
 </template>
 
 <style>
-  section {
-    display: grid;
-    margin: 2rem;
-    gap: 1rem;
-  }
+* {
+    padding: 0;
+    margin: 0;
+}
+main {
+    padding: 1em;
+}
 </style>
--- src/pages/PageTemperature.vue
+++ src/pages/PageTemperature.vue
@@ -1,11 +1,40 @@
 <script setup>
-  
+import { defineModel, ref } from "vue";
+import BaseInputNumber from "../components/BaseInputNumber.vue";
+
+const temperatureSi = ref(0); //En Kelvin
+
+const tempKelvin = defineModel("kelvin", {
+    get: () => parseFloat(temperatureSi.value.toFixed(2)),
+    set: (value) => (temperatureSi.value = Number(value)),
+});
+
+const tempCelsius = defineModel("celsius", {
+    get: () => parseFloat((temperatureSi.value - 273.15).toFixed(2)),
+    set: (value) => (temperatureSi.value = Number(value + 273.15)),
+});
+
+const tempFahrenheit = defineModel("fahrenheit", {
+    get: () =>
+        parseFloat((((temperatureSi.value - 273.15) * 9) / 5 + 32).toFixed(2)),
+    set: (value) =>
+        (temperatureSi.value = Number(((value - 32) * 5) / 9 + 273.15)),
+});
 </script>
 
 <template>
-  <h2>Temperature Converter</h2>
+    <h2>Temperature Converter</h2>
+    <section>
+        <BaseInputNumber label="Temperature in Kelvin" v-model="tempKelvin" />
+        <BaseInputNumber label="Temperature in Celsius" v-model="tempCelsius" />
+        <BaseInputNumber label="Temperature in Fahrenheit" v-model="tempFahrenheit" />
+    </section>
 </template>
 
 <style scoped>
-  
+section {
+    display: grid;
+    margin: 2rem;
+    gap: 1rem;
+}
 </style>
